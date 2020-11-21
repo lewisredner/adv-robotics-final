a349dd6d6989d3b4853165cf67b8037cccfbce6a
Srikanth817 <srikanth817@gmail.com>
2020-11-21 16:58:27 -0700

    Add files via upload
    Drive into wall

--- controllers/av_challenge_controller/av_challenge_controller.py
+++ controllers/av_challenge_controller/av_challenge_controller.py
@@ -2,15 +2,25 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot, Camera
+from controller import Robot, Camera, Motor, DistanceSensor
+import numpy as np
+import cv2
 
+from vehicle import Driver
+ 
 # create the Robot instance.
+#robot = Robot()
 robot = Driver()
 front_camera = robot.getCamera("front_camera")
 rear_camera = robot.getCamera("rear_camera")
-
+lidar = robot.getLidar("SickLms291")
+m1 = robot.getMotor("front right wheel")
+#for att in dir(robot):
+#    print(att,getattr(robot,att))
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
+print(timestep)
+#print(dir(robot))
 
 # You should insert a getDevice-like function in order to get the
 # instance of a device of the robot. Something like:
@@ -24,15 +34,15 @@ rear_camera.enable(30)
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
+while robot.step() != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
-
+    robot.setCruisingSpeed(1000)
+    robot.setSteeringAngle(10)
     # Process sensor data here.
 
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
     pass
 
-# Enter here exit cleanup code.
