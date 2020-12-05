9ecb16315fdd8775883eae5e2c6a544af7a38397
Jot Kaur <baljot.kaur@gmail.com>
2020-12-05 00:13:53 -0800

    added plotting for speed. Working on other attributes next. plotting set to false by default

--- controllers/av_challenge_controller/av_challenge_controller.py
+++ controllers/av_challenge_controller/av_challenge_controller.py
@@ -6,6 +6,7 @@ from controller import Robot, Camera, Motor, DistanceSensor
 import numpy as np
 import cv2 as cv
 import math
+import matplotlib.pyplot as plt
 
 from vehicle import Driver
  
@@ -15,6 +16,8 @@ robot = Driver()
 front_camera = robot.getCamera("front_camera")
 rear_camera = robot.getCamera("rear_camera")
 lidar = robot.getLidar("Sick LMS 291")
+
+MAKEPLOT = False # TODO next need to add plotting for things other than speed
 
 #for att in dir(robot):
 #    print(att,getattr(robot,att))
@@ -33,12 +36,42 @@ print(timestep)
 front_camera.enable(30)
 rear_camera.enable(30)
 
+'''
+def make_plot():
+    y = np.random.random()
+    plt.scatter(4, y)
+    plt.pause(0.05)
+    print("iterate")
+    plt.show()
+'''
+plt.style.use('ggplot')
+
+def live_plotter(x_vec, y1_data, change, title):
+    pause_time=0.1
+    if change == []:
+        plt.ion()
+        fig = plt.figure(figsize=(13,6))
+        ax = fig.add_subplot(111)
+        change, = ax.plot(x_vec, y1_data,'-o',alpha=0.8)        
+        plt.ylabel('Y Label')
+        plt.title(title)
+        plt.show()
+    
+    change.set_ydata(y1_data)
+
+    if np.min(y1_data)<=change.axes.get_ylim()[0] or np.max(y1_data)>=change.axes.get_ylim()[1]:
+        plt.ylim([np.min(y1_data)-np.std(y1_data),np.max(y1_data)+np.std(y1_data)])
+
+    plt.pause(pause_time)
+
+    return change
+
 def process_front(img):
     #Return slope and midpoint 
-    print(img.shape)           
+    print(img.shape)        
     #Crop image
     #Preprocessing in general could be improved alot               
-    img = img[42*2:64*2,48*2:80*2]
+    #img = img[42*2:64*2,48*2:80*2]
     # im = cv.imread('testimg.png')
     imgray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
     ret,thresh = cv.threshold(imgray,150,255,0)
@@ -95,6 +128,13 @@ def process_front(img):
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 steer = 0
+# for plot
+if MAKEPLOT is True:
+    size = 100 
+    x_vec = np.linspace(0, 1, size)
+    y_vec = np.linspace(0, 0, size)
+    change = []
+
 while robot.step() != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
@@ -105,6 +145,15 @@ while robot.step() != -1:
     # cv.namedWindow("main", cv.WINDOW_NORMAL)
     # cv.imshow('main', img_cv)
     # cv.waitKey()
+
+    # live plotting
+    if MAKEPLOT is True:
+        rand_val = np.random.randn(1)
+        y_vec[-1] = float(robot.getCurrentSpeed())
+        change = live_plotter(x_vec,y_vec,change, "Live Tesla Speed")
+        y_vec = np.append(y_vec[1:],0.0)
+
+
     nav_con = process_front(img_cv)
     #TODO: implement PID. I was too lazy last night
     if(nav_con!=-1):
